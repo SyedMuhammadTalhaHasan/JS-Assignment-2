52ba6bdb440c1777724155040451058f119fb282
SyedMuhammadTalhaHasan <155646073+SyedMuhammadTalhaHasan@users.noreply.github.com>
2024-01-21 11:02:13 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var length = 15;
+var width = 38;
+var area = length * width;
+console.log("Length of rectangle is : " + length);
+console.log("Width of rectangle is : " + width);
+console.log("Area of rectangle is : " + area);
+
+var userName = prompt("Please enter your name : ");
+alert("Hi and a huge welcome to Mr." + userName + " I'm happy to have you on my website.");
